a465fd4184bcbe512bcd7359f24b181f79f65eb8
xavier <xavier@192.168.1.12>
2019-05-28 11:10:19 -0500

    Conflicts: 	config/conexionBD.php

--- config/conexionBD.php
+++ config/conexionBD.php
@@ -1,13 +1,16 @@
-<?php
-$db_servername = "db4free.net:3306";
-$db_username = "pizzeria"; 
-$db_password = "pizzeria"; 
-$db_name = "pizzeriadb"; $
-$conn = new mysqli($db_servername, $db_username, $db_password, $db_name);
-$conn->set_charset("utf8");
-# Probar conexión 
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "<p>Conexión exitosa!! :)</p>";
-}
+<?php        
+
+    $servername = "db4free.net:3306";
+    $username = "pizzeria";
+    $password = "pizzeria";
+    $dbname = "pizzeriadb";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }else{
+    	//echo "Conexion exitosa !!";
+    }  
+?>
